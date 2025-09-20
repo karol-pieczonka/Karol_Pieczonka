1c1842f4a715e25dd12bb51358f84ec6111ca7df
Karol Pieczonka <karol.pieczonka@creativestyle.pl>
2025-09-20 19:32:50 +0200

    feat(core): Initialize project structure

---
+++ src/app/features/posts/post-details/post-details.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-details',
+  imports: [],
+  templateUrl: './post-details.html',
+})
+export class PostDetails {
+
+}
---
+++ src/app/features/posts/post-list/post-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  imports: [],
+  templateUrl: './post-list.html',
+})
+export class PostList {
+
+}
