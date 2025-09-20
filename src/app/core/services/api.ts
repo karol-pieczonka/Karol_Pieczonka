0cbd39422411e73ec0ee3091e1c4ee8b5ec7af31
Karol Pieczonka <karol.pieczonka@creativestyle.pl>
2025-09-20 19:57:07 +0200

    feat(core): add ApiService and configure HttpClient

--- src/app/core/services/api.ts
+++ src/app/core/services/api.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
+import { User } from '../models/user.model';
+import { Comment } from '../models/comment.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Api {
-  
+export class ApiService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'https://jsonplaceholder.typicode.com';
+
+  getPosts(userId?: number): Observable<Post[]> {
+    let params = new HttpParams();
+    if (userId) {
+      params = params.set('userId', userId.toString());
+    }
+    return this.http.get<Post[]>(`${this.apiUrl}/posts`, { params });
+  }
+
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>(`${this.apiUrl}/posts/${id}`);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${id}`);
+  }
+
+  getComments(postId: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${this.apiUrl}/posts/${postId}/comments`);
+  }
 }
